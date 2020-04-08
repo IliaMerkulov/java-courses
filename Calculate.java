920de456cc042209c4331ac8df5a74051baab5fb
IliaMerkulov <Merkulov.it@gmail.com>
2020-04-08 22:23:14 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,6 @@
 public class Calculate {
     public static void main(String[] args) {
-        System.out.println("Calculate");
+        //System.out.println("Calculate");
         int first = Integer.valueOf(args[0]);
         int second = Integer.valueOf(args[1]);
         int sum = first + second;
@@ -13,5 +13,8 @@ public class Calculate {
         System.out.println("del "+del);
         int stepen = (int)Math.pow(first, second);
         System.out.println("stepen "+stepen);
+
+
+
     }
 }
